47923ba3fa04159c39c5b5d9493c0d16b730c05f
Tiago Vattos Zamboni <tiagovattosz@gmail.com>
2025-09-06 16:52:14 -0300

    Alterado para remover cargo de funcionarios ao excluir cargo

--- src/main/java/br/edu/fema/gympro/service/CargoService.java
+++ src/main/java/br/edu/fema/gympro/service/CargoService.java
@@ -1,10 +1,12 @@
 package br.edu.fema.gympro.service;
 
 import br.edu.fema.gympro.domain.Cargo;
+import br.edu.fema.gympro.domain.Funcionario;
 import br.edu.fema.gympro.dto.cargo.CargoRequestDTO;
 import br.edu.fema.gympro.dto.cargo.CargoResponseDTO;
 import br.edu.fema.gympro.repository.CargoRepository;
 import br.edu.fema.gympro.exception.domain.ObjetoNaoEncontrado;
+import br.edu.fema.gympro.repository.FuncionarioRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,9 +15,11 @@ import java.util.List;
 @Service
 public class CargoService {
     private final CargoRepository cargoRepository;
+    private final FuncionarioRepository funcionarioRepository;
 
-    public CargoService(CargoRepository cargoRepository) {
+    public CargoService(CargoRepository cargoRepository, FuncionarioRepository funcionarioRepository) {
         this.cargoRepository = cargoRepository;
+        this.funcionarioRepository = funcionarioRepository;
     }
 
     public List<CargoResponseDTO> findAll() {
@@ -49,6 +53,12 @@ public class CargoService {
     public void deleteById(Long id) {
         if (!cargoRepository.existsById(id)) {
             throw new ObjetoNaoEncontrado("Cargo não encontrado!");
+        }
+        Cargo cargo = findCargoOrThrow(id);
+        List<Funcionario> funcionarios = funcionarioRepository.findByCargo(cargo);
+        for(Funcionario funcionario : funcionarios) {
+            funcionario.setCargo(null);
+            funcionarioRepository.save(funcionario);
         }
         cargoRepository.deleteById(id);
     }
